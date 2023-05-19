8cefbe9b8c2bc194693e6b1f83a9832dd36ea49b
batat <z.marganova@gmail.com>
2023-05-19 23:50:33 +0300

    Progress in the development of the fenceContact function

--- core/src/main/kotlin/com/github/marzr/castles/geometry/Edge.kt
+++ core/src/main/kotlin/com/github/marzr/castles/geometry/Edge.kt
@@ -1,5 +1,7 @@
 package com.github.marzr.castles.geometry
 
+import com.github.marzr.castles.geometry.Position.Rotation.*
+
 sealed interface Edge
 data class Horizontal(val y: Int, val x1: Int, val x2: Int) : Edge
 data class Vertical(val x: Int, val y1: Int, val y2: Int) : Edge
@@ -8,52 +10,35 @@ fun PositionedTile.top(): Edge = when (val f = this.toFigure()) {
     is Circle -> f.top()
     is Rectangle -> f.top()
     is Octagon -> f.top()
+    is LFigure -> TODO("is not supposed to invoke")
 }
 
 fun Circle.top() = when (position.rotation) {
-    Position.Rotation.R0 -> Horizontal(
-        y = position.y,
-        x1 = position.x + diameter / 2,
-        x2 = position.x + diameter / 2 + 1
-    )
-
-    Position.Rotation.R90 -> Vertical(
-        x = position.x + diameter,
-        y1 = position.y - diameter / 2 - 1,
-        y2 = position.y - diameter / 2
-    )
-
-    Position.Rotation.R180 -> Horizontal(
-        y = position.y - diameter,
-        x1 = position.x + diameter / 2,
-        x2 = position.x + diameter / 2 + 1
-    )
-
-    Position.Rotation.R270 -> Vertical(
-        x = position.x,
-        y1 = position.y - diameter / 2 - 1,
-        y2 = position.y - diameter / 2
-    )
+    R0 -> Horizontal(y = position.y, x1 = position.x + diameter / 2, x2 = position.x + diameter / 2 + 1)
+    R90 -> Vertical(x = position.x + diameter, y1 = position.y - diameter / 2 - 1, y2 = position.y - diameter / 2)
+    R180 -> Horizontal(y = position.y - diameter, x1 = position.x + diameter / 2, x2 = position.x + diameter / 2 + 1)
+    R270 -> Vertical(x = position.x, y1 = position.y - diameter / 2 - 1, y2 = position.y - diameter / 2)
 }
 
 fun Rectangle.top() = when (position.rotation) {
-    Position.Rotation.R0 -> Horizontal(y = position.y, x1 = position.x, x2 = position.x + width)
-    Position.Rotation.R90 -> Vertical(x = position.x + width, y1 = position.y - height, y2 = position.y)
-    Position.Rotation.R180 -> Horizontal(y = position.y - width, x1 = position.x, x2 = position.x + width)
-    Position.Rotation.R270 -> Vertical(x = position.x, y1 = position.y - width, y2 = position.y)
+    R0 -> Horizontal(y = position.y, x1 = position.x, x2 = position.x + width)
+    R90 -> Vertical(x = position.x + width, y1 = position.y - height, y2 = position.y)
+    R180 -> Horizontal(y = position.y - width, x1 = position.x, x2 = position.x + width)
+    R270 -> Vertical(x = position.x, y1 = position.y - width, y2 = position.y)
 }
 
 fun Octagon.top() = when (position.rotation) {
-    Position.Rotation.R0 -> Horizontal(y = position.y, x1 = position.x + 1, x2 = position.x + width - 1)
-    Position.Rotation.R90 -> Vertical(x = position.x + width, y1 = position.y - 1, y2 = position.y - height + 1)
-    Position.Rotation.R180 -> Horizontal(y = position.y - width, x1 = position.x + width - 1, x2 = position.x + 1)
-    Position.Rotation.R270 -> Vertical(x = position.x, y1 = position.y - width + 1, y2 = position.y - 1)
+    R0 -> Horizontal(y = position.y, x1 = position.x + 1, x2 = position.x + width - 1)
+    R90 -> Vertical(x = position.x + width, y1 = position.y - 1, y2 = position.y - height + 1)
+    R180 -> Horizontal(y = position.y - width, x1 = position.x + width - 1, x2 = position.x + 1)
+    R270 -> Vertical(x = position.x, y1 = position.y - width + 1, y2 = position.y - 1)
 }
 
 fun PositionedTile.edges() = when (val f = this.toFigure()) {
     is Circle -> f.edges()
     is Octagon -> f.edges()
     is Rectangle -> f.edges()
+    is LFigure -> f.edges()
 }
 
 fun Circle.edges() = Position.Rotation.values().map {
@@ -61,7 +46,7 @@ fun Circle.edges() = Position.Rotation.values().map {
 }
 
 fun Rectangle.edges() =
-    if (position.rotation == Position.Rotation.R0 || position.rotation == Position.Rotation.R180) {
+    if (position.rotation == R0 || position.rotation == R180) {
         listOf(
             Horizontal(y = position.y, x1 = position.x, x2 = position.x + width),
             Vertical(x = position.x, y1 = position.y - height, y2 = position.y),
@@ -77,19 +62,30 @@ fun Rectangle.edges() =
         )
     }
 
-fun Octagon.edges() =
-    if (position.rotation == Position.Rotation.R0 || position.rotation == Position.Rotation.R180) {
+fun Octagon.edges() = with(position) {
+    if (rotation == R0 || rotation == R180) listOf(
+        Horizontal(y = y, x1 = x + 1, x2 = x + width - 1),
+        Vertical(x = x, y1 = y - height + 1, y2 = y - 1),
+        Horizontal(y = y - height, x1 = x + 1, x2 = x + width - 1),
+        Vertical(x = x + width, y1 = y - height + 1, y2 = y - 1)
+    )
+    else listOf(
+        Horizontal(y = y, x1 = x + 1, x2 = x + height - 1),
+        Vertical(x = x, y1 = y - width + 1, y2 = y - 1),
+        Horizontal(y = y - width, x1 = x + 1, x2 = x + height - 1),
+        Vertical(x = x + height, y1 = y - width + 1, y2 = y - 1)
+    )
+}
+
+fun LFigure.edges() = when (position.rotation) {
+    R0 -> with(position) {
         listOf(
-            Horizontal(y = position.y, x1 = position.x + 1, x2 = position.x + width - 1),
-            Vertical(x = position.x, y1 = position.y - height + 1, y2 = position.y - 1),
-            Horizontal(y = position.y - height, x1 = position.x + 1, x2 = position.x + width - 1),
-            Vertical(x = position.x + width, y1 = position.y - height + 1, y2 = position.y - 1)
-        )
-    } else {
-        listOf(
-            Horizontal(y = position.y, x1 = position.x + 1, x2 = position.x + height - 1),
-            Vertical(x = position.x, y1 = position.y - width + 1, y2 = position.y - 1),
-            Horizontal(y = position.y - width, x1 = position.x + 1, x2 = position.x + height - 1),
-            Vertical(x = position.x + height, y1 = position.y - width + 1, y2 = position.y - 1)
+            Horizontal(y, x, x + 2), Vertical(x + 2, y - 2, y), Horizontal(y + 2, x + 2, x + 4),
+            Vertical(x + 4, y + 2, y + 4), Horizontal(y + 4, x, x + 4), Vertical(x, y, y + 4)
         )
     }
+
+    R90 -> TODO()
+    R180 -> TODO()
+    R270 -> TODO()
+}
--- core/src/main/kotlin/com/github/marzr/castles/geometry/Figure.kt
+++ core/src/main/kotlin/com/github/marzr/castles/geometry/Figure.kt
@@ -1,8 +1,6 @@
 package com.github.marzr.castles.geometry
 
-import com.github.marzr.castles.data.FigureType
-import com.github.marzr.castles.data.Foyer
-import com.github.marzr.castles.data.Stairs
+import com.github.marzr.castles.data.*
 import com.github.marzr.castles.data.rooms.RoomTile
 
 sealed interface Figure
@@ -13,11 +11,13 @@ data class Rectangle(val width: Int, val height: Int, val position: Position) : 
 
 data class Circle(val diameter: Int, val position: Position) : Figure
 
+data class LFigure(val position: Position) : Figure
+
 fun PositionedTile.toFigure(): Figure = when (val tile = this.tile) {
     is RoomTile -> tile.toFigure(position)
     is Foyer -> Octagon(3, 3, position)
     is Stairs -> Rectangle(3, 1, position)
-    else -> TODO()
+    is Hallway -> TODO()
 }
 
 fun RoomTile.toFigure(position: Position): Figure = when (this.figureType) {
@@ -26,7 +26,7 @@ fun RoomTile.toFigure(position: Position): Figure = when (this.figureType) {
     FigureType.SQUARE -> Rectangle(4, 4, position)
     FigureType.SMALL_SQUARE -> Rectangle(2, 2, position)
     FigureType.LARGE_OCTAGON -> Octagon(7, 4, position)
-    FigureType.L_ROOM -> TODO()
+    FigureType.L_ROOM -> LFigure(position)
     FigureType.LARGE_RECTANGLE -> Rectangle(6, 3, position)
     FigureType.LONG_RECTANGLE -> Rectangle(7, 2, position)
     FigureType.MIDDLE_RECTANGLE -> Rectangle(5, 2, position)
