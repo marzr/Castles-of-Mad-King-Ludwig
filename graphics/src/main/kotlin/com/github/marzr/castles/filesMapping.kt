01a6115a702180765d254c862310768aa81d2ce6
batat <z.marganova@gmail.com>
2023-05-26 21:16:49 +0300

    Add rooms to mapping

--- graphics/src/main/kotlin/com/github/marzr/castles/filesMapping.kt
+++ graphics/src/main/kotlin/com/github/marzr/castles/filesMapping.kt
@@ -2,30 +2,24 @@ package com.github.marzr.castles
 
 val filesMap = mapOf(
     "Театр" to "Activity/BC_Theater.png",
-    "Зал заседаний" to "",
+    // "Зал заседаний" to "",
     "Кегельбан" to "Activity/LR_kreg.png",
     "Игровая" to "Activity/LR_train.png",
     "Бильярдная" to "Activity/MR_bil.png",
     "Часовня" to "Activity/MR_oratory.png",
     "Ателье" to "Activity/MR_sewing_room.png",
     "Зал приемов" to "Activity/Oct_Aud.png",
-    "Певческий зал" to "",
+    // "Певческий зал" to "",
     "Фортепианная" to "Activity/SC_Piano.png",
-    "Кабинет Берты" to "",
-    "Музыкальный класс" to "",
+    //"Кабинет Берты" to "",
+    // "Музыкальный класс" to "",
 
     "Верхний зал" to "Corridor/great_hall.png",
     "Главный зал" to "Corridor/upper_hall.png",
 
-    "Потайная комната" to "Utility/L_panic_room.png",
-
     "Темница" to "Downstairs/SC_the_hole.png",
 
     "Буфетная" to "Food/L_scullery.png",
-
-    "Хижина Хундинга" to "Outdoor/BC_hunding.png",
-    "Террасы" to "Outdoor/Oct_terrace.png",
-    "Парадные сады" to "Outdoor/LR_formal_gardens.png",
 
     "Западный гобеленный зал" to "Living tooms/LR_west_tapestry_room.png",
     "Художественный класс" to "Living tooms/LR_drawing_room.png",
@@ -42,7 +36,26 @@ val filesMap = mapOf(
     "Розовый кабинет" to "Living tooms/SSq_pink_cabinet.png",
     "Комната для медитации" to "Living tooms/SSq_focus_room.png",
 
+    "Хижина Хундинга" to "Outdoor/BC_hunding.png",
+    "Теплица" to "Outdoor/BC_oranzeria.png",
+    "Парадные сады" to "Outdoor/LR_formal_gardens.png",
+    "Каретный гараж" to "Outdoor/LR_carriage_garage.jpg",
+    "Привратницкая" to "Outdoor/MR_gatekeeper_TODO.png",
+    "Крыльцо" to "Outdoor/MR_porch.png",
+    "Сарай" to "Outdoor/MR_shed.png",
+    "Террасы" to "Outdoor/Oct_terrace.png",
+    "Конюшни" to "Outdoor/Oct_stables.png",
+    //"Французская беседка" to "Outdoor/Sq_pavilion.png", TODO
+    "Сад тыкв" to "Outdoor/Sq_pumpkin_garden.png",
+
+    //"" to "image/Sleeping/L_krola.png", TODO
     "Спальня королевы" to "Sleeping/L_queen.png",
+    "Комната Тассо" to "Sleeping/L_tasso.png",
     "Большая спальня" to "Sleeping/Sq_grand_bedchamber.png",
+    "Комната для слуг" to "Sleeping/Sq_servants_quarters.png",
     "Комната отдыха" to "Sleeping/SR_nap_room.png",
+    "Солярий" to "Sleeping/SR_solar.png",
+    "Гардеробная" to "Sleeping/SR_wardrobe_Copied_Fixed_TODO.png",
+
+    "Потайная комната" to "Utility/L_panic_room.png",
 )
